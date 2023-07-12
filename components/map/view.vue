f4cda982b075b725b8c865d439c00ac0a4c97998
ikanexus <git+github@ika.gg>
2023-07-12 11:17:06 +0100

    add leaf marker

--- components/map/view.vue
+++ components/map/view.vue
@@ -22,8 +22,19 @@ onMounted(() => {
         maxZoom: 10
     }).addTo(map);
 
+    const greenIcon = L.icon({
+        iconUrl: '/images/leaflet/leaf-green.png',
+        shadowUrl: '/images/leaflet/leaf-shadow.png',
+
+        iconSize:     [38, 95], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
     props.markers.forEach(entry => {
-        const marker = L.marker(entry.coords).addTo(map);
+        const marker = L.marker(entry.coords, { icon: greenIcon }).addTo(map);
         marker.bindPopup(`<img src="${entry.image}">`, {
             minWidth: 256
         });
