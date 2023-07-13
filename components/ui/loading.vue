99d1203beceff2ec7f91047bc98dfcb71ddfe379
Tougrel <lambrosagris@gmail.com>
2023-07-13 20:24:19 +0000

    refactor(loading): changed loading gif

--- components/ui/loading.vue
+++ components/ui/loading.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="fixed bg-navigation inset-0 flex items-center justify-center z-500">
-        <img src="/static/images/emotes/waddle.gif" decoding="async" loading="lazy" :preload="true" class="bg-repeat-x bg-cover" />
+    <div class="fixed bg-white dark:bg-navigation inset-0 flex items-center justify-center z-500">
+        <img src="/static/images/emotes/dance.gif" decoding="async" loading="lazy" :preload="true"
+            class="bg-repeat-x bg-cover" />
     </div>
 </template>
