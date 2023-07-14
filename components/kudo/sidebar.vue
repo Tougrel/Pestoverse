669f4b5d87bf5e3f90aff1ea937da0ed277dd4f0
Tougrel <lambrosagris@gmail.com>
2023-07-14 13:01:10 +0000

    refactor(sidebar): moved button on the bottom of the slideover and removed it from desktop monitors

--- components/kudo/sidebar.vue
+++ components/kudo/sidebar.vue
@@ -4,11 +4,14 @@ const isOpen = useState<boolean>("kudo-slideover");
 
 <template>
     <USlideover v-model="isOpen">
-        <div class="flex flex-col gap-4 p-4">
-            <button class="text-lg font-bold bg-red-700 dark:bg-red-500 p-2 rounded-lg" @click="isOpen = false">CLOSE</button>
+        <div class="flex flex-col justify-between gap-4 h-full p-4">
+            <div class="flex flex-col gap-4">
+                <KudoCard v-for="kudo in KUDO_BOARDS" :title="kudo.title" :description="kudo.description" :icon="kudo.icon"
+                    :to="kudo.to" />
+            </div>
 
-            <KudoCard v-for="kudo in KUDO_BOARDS" :title="kudo.title" :description="kudo.description" :icon="kudo.icon"
-                :to="kudo.to" />
+            <button class="lg:hidden text-lg font-bold bg-red-700 dark:bg-red-500 p-2 rounded-lg"
+                @click="isOpen = false">CLOSE</button>
         </div>
     </USlideover>
 </template>
