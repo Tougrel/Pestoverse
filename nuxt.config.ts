0a5d2f0989034f253d24bb55c215c4b3981e7b68
ikanexus <git+github@ika.gg>
2023-07-15 10:10:24 +0100

    fix: remove @nuxt/content to see if that's causing deploy failures

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 		enabled: true,
 	},
 
-	modules: ["@nuxt/devtools", "@nuxt/image", "@nuxthq/ui", "nuxt-icon", "@nuxt/content"],
+	modules: ["@nuxt/devtools", "@nuxt/image", "@nuxthq/ui", "nuxt-icon"],
 
 	css: ["~/assets/css/index.css"],
 
