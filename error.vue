3b1bb8282c091336a2dcdde39f2bd0909b5abe41
ikanexus <git+github@ika.gg>
2023-07-15 18:32:15 +0100

    feat: zoom dog in and make unselectable

--- error.vue
+++ error.vue
@@ -7,10 +7,23 @@ import dog from 'static/images/dog.png';
 const handleErrors = () => clearError({ redirect: "/" });
 </script>
 
+<style>
+@keyframes scalein {
+  0% {
+      transform: scale(1.0);
+      -webkit-transform: scale(1.0);
+  }
+  100% {
+      transform: scale(2.0);
+      -webkit-transform: scale(2.0);
+  }
+}
+</style>
+
 <template>
     <div class="flex flex-col items-center justify-center gap-4 p-4 h-screen">
         <img :src="scream" decoding="async" loading="lazy" alt="Scream" class="rounded-lg" />
-        <div class="text-center">
+        <div class="text-center z-10">
             <h1 class="text-3xl lg:text-6xl font-bold text-red-600 dark:text-red-500">BLACK HOLE ALERT</h1>
             <p class="text-lg text-gray-600 dark:text-gray-400">
                 The universe is about to collapse due to an unexpected error!
@@ -20,12 +33,12 @@ const handleErrors = () => clearError({ redirect: "/" });
             </p>
         </div>
         <code
-            class="bg-gray-300 dark:bg-navigation/40 border-2 border-navigation p-2 rounded-lg max-w-screen-2xl w-full">{{ error }}</code>
+            class="bg-gray-300 dark:bg-navigation/40 border-2 border-navigation p-2 rounded-lg max-w-screen-2xl w-full z-10">{{ error }}</code>
         <button @click="handleErrors"
-            class="border-2 border-green-700 px-4 py-2 rounded-lg transition-all hover:bg-green-300 hover:dark:bg-green-900 hover:scale-105 duration-300">
+            class="border-2 border-green-700 px-4 py-2 rounded-lg transition-all hover:bg-green-300 hover:dark:bg-green-900 hover:scale-105 duration-300 z-10">
             Try to restore the Universe
         </button>
-        <img :src="dog" class="fixed bottom-0 left-0 opacity-5 grayscale" />
+        <img :src="dog" class="fixed bottom-0 left-0 opacity-5 grayscale z-0 animate-[scalein_60s_linear_normal]" draggable="false" />
     </div>
 </template>
   
