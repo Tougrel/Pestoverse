9d5fc022146a6acfbbb118e5ecb92386b234c1d1
ikanexus <git+github@ika.gg>
2023-07-15 17:14:06 +0100

    feat: add cat

---
+++ components/easter-egg/button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import cat from 'static/images/cat.webp';
+import nya from 'static/audio/nya.mp3';
+
+const playNya = () => {
+    const audio = new Audio(nya);
+    audio.play();
+} 
+
+</script>
+
+<template>
+    <button @click="playNya">
+        <img :src="cat" loading="lazy" decoding="async" class="fixed bottom-0 left-0 h-10 opacity-10 hover:opacity-60" />
+    </button>
+</template>
