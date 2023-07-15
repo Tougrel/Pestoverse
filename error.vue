54ddeadd71e963f4e5f24a1730c79ddb158d723d
ikanexus <git+github@ika.gg>
2023-07-15 18:07:19 +0100

    feat: add dog on error page

--- error.vue
+++ error.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 defineProps<{ error: Object; }>();
 
+import scream from 'static/images/emotes/scream.webp';
+import dog from 'static/images/dog.png';
+
 const handleErrors = () => clearError({ redirect: "/" });
 </script>
 
 <template>
     <div class="flex flex-col items-center justify-center gap-4 p-4 h-screen">
-        <img src="static/images/emotes/scream.webp" decoding="async" loading="lazy" alt="Scream" class="rounded-lg" />
+        <img :src="scream" decoding="async" loading="lazy" alt="Scream" class="rounded-lg" />
         <div class="text-center">
             <h1 class="text-3xl lg:text-6xl font-bold text-red-600 dark:text-red-500">BLACK HOLE ALERT</h1>
             <p class="text-lg text-gray-600 dark:text-gray-400">
@@ -22,6 +25,7 @@ const handleErrors = () => clearError({ redirect: "/" });
             class="border-2 border-green-700 px-4 py-2 rounded-lg transition-all hover:bg-green-300 hover:dark:bg-green-900 hover:scale-105 duration-300">
             Try to restore the Universe
         </button>
+        <img :src="dog" class="fixed bottom-0 left-0 opacity-5 grayscale" />
     </div>
 </template>
   
