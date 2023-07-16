a0d16a0308b535bfdfd36fbf68949e4bc1db7e3f
ikanexus <git+github@ika.gg>
2023-07-16 17:28:57 +0100

    fix: reduce nya volume

--- components/easter-egg/button.vue
+++ components/easter-egg/button.vue
@@ -4,12 +4,13 @@ import nya from 'static/audio/nya.mp3';
 
 const playNya = () => {
     const audio = new Audio(nya);
+    audio.volume = 0.5;
     audio.play();
 } 
 </script>
 
 <template>
     <button @click="playNya" class="fixed bottom-0 left-0">
-        <img :src="cat" loading="lazy" decoding="async" class="h-10 opacity-20 hover:opacity-60" />
+        <img :src="cat" loading="lazy" decoding="async" class="h-12 opacity-20 hover:opacity-60" />
     </button>
 </template>
