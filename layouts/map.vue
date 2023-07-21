6e1938d662816e05fb729ea0008e93380872f3d4
ikanexus <git+github@ika.gg>
2023-07-21 19:55:22 +0100

    fix: make map not zoomable on mobile

--- layouts/map.vue
+++ layouts/map.vue
@@ -1,4 +1,5 @@
 <template>
+    <Meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no" />
     <div class="h-screen">
         <UiNavMenu class="absolute right-5 top-5" fontSize="sm" iconSize="1.5em" buttonClass="bg-white/60 dark:bg-black/60 rounded-lg p-2" :map="true" />
         <slot />
