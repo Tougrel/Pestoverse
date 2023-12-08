022bddb7e0d5afddfb24b2e8fbcf930037527025
Tougrel <lambrosagris@gmail.com>
2023-12-08 17:05:28 +0200

    fix: remove unnecassary code

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,5 @@
         <div class="flex flex-col items-center gap-4">
             <EasterEggSun />
         </div>
-        <!--<p class="text-4xl font-bold text-red-500">Foodge time brb!</p>-->
     </NuxtLayout>
 </template>
