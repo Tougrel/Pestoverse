e891c312ac10c5214a4b5d465a167d25bd5bcfbc
ikanexus <git+github@ika.gg>
2024-01-10 23:18:12 +0000

    feat: move state + secondary retrieval into functions to clean up a little

--- pages/awards.vue
+++ pages/awards.vue
@@ -5,23 +5,35 @@ const { data: categories } = await useFetch("/api/submissions/categories");
 type SubmissionState = { [key: number]: string };
 
 const mode = ref<string>("votes");
-let state = reactive<SubmissionState>({});
-let secondary = reactive([]); // TODO: rename this lmao
-if (status.value === "authenticated") {
-    if (mode.value === "suggestions") {
-        const { data: submissions } = await useFetch<SubmissionState>("/api/submissions");
-        const { data: names } = await useFetch("/api/submissions/names");
+const isAuthenticated = computed(() => status.value === "authenticated");
 
-        state = submissions.value || {};
-        secondary = names.value || [];
-    } else if (mode.value === "votes") {
-        const { data: votes } = await useFetch<SubmissionState>("/api/votes");
-        const { data: options } = await useFetch("/api/votes/options");
-
-        state = votes.value || {};
-        secondary = options.value || {};
+async function getState(mode: Ref<string>): Promise<SubmissionState> {
+    let result = {};
+    if (isAuthenticated.value) {
+        const { data } = await useFetch<SubmissionState>(`/api/${mode.value}`);
+        result = data.value || {};
     }
+    return result;
 }
+async function getSecondary(mode: Ref<string>) {
+    let result = [];
+    switch (mode.value) {
+        case "submissions":
+            if (isAuthenticated.value) {
+                const { data } = await useFetch("/api/submissions/names");
+                result = data.value || [];
+            }
+            break;
+        case "votes":
+            const { data } = await useFetch("/api/votes/options");
+            result = data.value || {};
+            break;
+    }
+    return result;
+}
+
+let state = reactive<SubmissionState>(await getState(mode));
+let secondary = reactive(await getSecondary(mode)); // TODO: rename this lmao
 
 const toast = useToast();
 const onSubmit = async () => {
@@ -62,11 +74,11 @@ const onSubmit = async () => {
                     title="Voting submissions"
                     description="Please check if the pestie you are voting for is in the suggested list first and use the same name!"
                 />
-                <UAlert v-if="status !== 'authenticated'" color="red" title="Authentication" description="You must login before you can vote!" />
+                <UAlert v-if="!isAuthenticated" color="red" title="Authentication" description="You must login before you can vote!" />
             </div>
             <AwardsSubmissions v-if="mode === 'submissions'" :categories="categories" :state="state" :names="secondary" />
             <AwardsVotes v-if="mode === 'votes'" :categories="categories" :state="state" :options="secondary" />
-            <UButton block type="submit" label="Submit" icon="i-mdi-check" size="lg" :disabled="status !== 'authenticated'" />
+            <UButton block type="submit" label="Submit" icon="i-mdi-check" size="lg" :disabled="!isAuthenticated" />
         </UForm>
     </NuxtLayout>
 </template>
