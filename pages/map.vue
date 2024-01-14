ed20a3f40e26078e7bddbc5e5f59969f5087d9df
ikanexus <git+github@ika.gg>
2024-01-14 17:38:32 +0000

    feat: add loading waddler to map view until idle

--- pages/map.vue
+++ pages/map.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { MarkerProps } from "@types/pestoverse";
+import type { MarkerProps } from "@pestoverse/types";
 
 const images = await queryContent("gallery").only(["coords", "images", "name"]).find();
 
@@ -17,13 +17,23 @@ const transformMarkers = () => {
     return markers;
 };
 
+const loading = ref(true);
+
+const loaded = () => {
+    console.log('loaded')
+    loading.value = false
+}
+
 const markers = transformMarkers();
 </script>
 
 <template>
     <NuxtLayout name="map">
+        <div v-show="loading" class="flex h-full w-full flex-col items-center justify-center">
+            <img src="/static/images/emotes/waddle.gif" decoding="async" loading="lazy" class="bg-cover bg-repeat-x" />
+        </div>
         <ClientOnly>
-            <MapView :markers="markers" />
+            <MapView :markers="markers" @loaded="loaded" />
         </ClientOnly>
     </NuxtLayout>
 </template>
--- pages/v2/index.vue
+++ pages/v2/index.vue
@@ -16,6 +16,13 @@ const transformMarkers = () => {
     return markers;
 };
 
+const loading = ref(true);
+
+const loaded = () => {
+    console.log('loaded')
+    loading.value = false
+}
+
 const markers = transformMarkers();
 
 definePageMeta({
@@ -23,12 +30,16 @@ definePageMeta({
     middleware: ["new-ui"],
     devs_only: true,
 });
+
 </script>
 
 <template>
     <NuxtLayout name="new">
+        <div v-show="loading" class="flex h-full w-full flex-col items-center justify-center">
+            <img src="/static/images/emotes/waddle.gif" decoding="async" loading="lazy" class="bg-cover bg-repeat-x" />
+        </div>
         <ClientOnly>
-            <MapView :markers="markers" />
+            <MapView :markers="markers" @loaded.once="loaded" />
         </ClientOnly>
     </NuxtLayout>
 </template>
