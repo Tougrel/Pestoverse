f8abc83aad515cff2f849cdf6fffe009a2c0d1f7
Tougrel <lambrosagris@gmail.com>
2024-01-14 17:49:17 +0200

    feat: mobile navigation

--- layouts/new.vue
+++ layouts/new.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+const slide = ref(false);
+</script>
+
 <template>
     <div class="flex h-screen flex-row">
-        <UiNavigation />
+        <UButton icon="i-mdi-menu" size="lg" class="flex lg:hidden fixed bottom-0 left-0 m-2 z-10" @click="slide = true"/>
+        <USlideover v-model="slide" side="left" :ui="{ base: '!flex-none', width: '!w-fit' }">
+            <UiNavigation class="h-full" />
+        </USlideover>
+        <UiNavigation class="hidden lg:flex" />
         <main class="w-full overflow-y-auto">
             <slot />
         </main>
