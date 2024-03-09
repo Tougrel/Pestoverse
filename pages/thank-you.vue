3f766ff3887b44a923eeb209f2970b49dd75ec2a
ikanexus <git+github@ika.gg>
2024-03-09 13:15:40 +0000

    feat: add fullscreen and other youtube allows back

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -75,9 +75,12 @@ onMounted(() => {
     </div>
     <div v-show="!loading" class="columns-1 md:columns-2 px-4 pb-4">
         <iframe class="w-full h-auto [aspect-ratio:16/9] rounded-lg" src="https://www.youtube.com/embed/qnumxLjtrJI"
-            frameborder="0"></iframe>
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
         <video controls class="w-full h-auto [aspect-ratio:16/9] rounded-lg">
-            <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type="video/mp4" />
+            <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type="video/mp4"
+                allowfullscreen />
         </video>
     </div>
 </template>
